79d3ce8f289894ba2185ac57908010f2212a4432
lvorcool <wang.nlqf@163.com>
2021-07-01 20:44:48 +0800

    增加user接口、配置swagger接口文档

--- bbsweb/bbsweb/urls.py
+++ bbsweb/bbsweb/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BBS WEB API",
+        default_version='v1',
+        description="论坛系统接口文档",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    # 配置swagger接口文档地址
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # 配置业务接口
     path('admin/', admin.site.urls),
+    path('', include('usersmanage.urls'))
 ]
--- bbsweb/usersmanage/models.py
+++ bbsweb/usersmanage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# TODO
+'''
+第一步在此建 user 表模型
+第二步 setting.py 增加 usersmanage.apps.UsersmanageConfig
+第三步 python manage.py makemigrations usersmanage
+第四步 python manage.py migrate
+'''
+
+
